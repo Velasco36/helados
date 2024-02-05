71d145df4327a14c793538408b21b8e8d1a82e01
Miguel <migue16velasco@gmail.com>
2024-02-05 12:32:08 -0400

    new moles

--- applications/products/models.py
+++ applications/products/models.py
@@ -1,35 +1,24 @@
 from django.db import models
 
-class Classification(models.Model):
-
-    name = models.CharField(max_length=50)
+# Create your models here.
+class Clasification(models.Model):
+    name = models.CharField(max_lenght=50)
 
 class Paleta(models.Model):
-
-    classification = models.ForeignKey(Classification, on_delete=models.CASCADE)
-
-    name = models.CharField(max_length=50)
-
+    clasification = models.ForeignKey(Clasification, on_delete=models.CASCADE)
+    name = models.CharField(max_lenght=50)
     description = models.TextField()
-
     price = models.FloatField()
 
 class RawMaterialInventory(models.Model):
-
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_lenght=50)
 
 class MerchandiseMovement(models.Model):
-
-    inventory = models.ForeignKey(RawMaterialInventory, on_delete=models.CASCADE)
-
+    inventory = models.Foren(RawMaterialInventory, on_delete=models.CASCADE)
     initial_inventory = models.IntegerField()
-
     final_inventory = models.IntegerField()
-
     movement_date = models.DateTimeField()
 
 class PaletaInventory(models.Model):
-
     paleta = models.ForeignKey(Paleta, on_delete=models.CASCADE)
-
     quantity = models.IntegerField()
